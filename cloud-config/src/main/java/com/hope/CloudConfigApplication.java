c3651aaf6980eb479bf99fdbc066a36a7e220bde
qq18281714982 <java@aodeng.cc>
2019-04-21 11:40:27 +0800

    :tada:

--- cloud-config/src/main/java/com/hope/CloudConfigApplication.java
+++ cloud-config/src/main/java/com/hope/CloudConfigApplication.java
@@ -4,10 +4,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableConfigServer
+//@EnableConfigServer
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CloudConfigApplication {
 
 	/** logger */
@@ -16,7 +17,7 @@ public class CloudConfigApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CloudConfigApplication.class, args);
 		LOGGER.info("=============================》");
-		LOGGER.info("=============================》hope-plus-cloud-config 外部配置启动成功！温馨提示：代码千万行，注释第一行，命名不规范，同事泪两行");
+		LOGGER.info("=============================》hope-cloud-config 外部配置启动成功！温馨提示：代码千万行，注释第一行，命名不规范，同事泪两行");
 		LOGGER.info("=============================》");
 	}
 
